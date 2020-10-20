3b72b0f6b30e97fdc7c40907ce53e76dcb1e1b9c
matheus <matheus2060@gmail.com>
2020-10-20 20:10:38 -0300

    feat(users): users table added in the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_220529) do
+ActiveRecord::Schema.define(version: 2020_10_20_230240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,18 @@ ActiveRecord::Schema.define(version: 2020_10_14_220529) do
     t.text "exercises"
     t.bigint "workout_id", null: false
     t.index ["workout_id"], name: "index_days_on_workout_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "cpf"
+    t.date "birth_date"
+    t.string "phone"
+    t.string "gender"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "workouts", force: :cascade do |t|
