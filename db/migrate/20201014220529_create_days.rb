c56569621985c23aa3a2e5c2bdf0dc983c469139
Igor Antun <igor@antun.me>
2020-10-24 19:28:24 -0300

    fix: non-null fields on models

--- db/migrate/20201014220529_create_days.rb
+++ db/migrate/20201014220529_create_days.rb
@@ -1,7 +1,7 @@
 class CreateDays < ActiveRecord::Migration[6.0]
   def change
     create_table :days do |t|
-      t.string :name
+      t.string :name, null: false
       t.belongs_to :workout, null: false, foreign_key: true
     end
   end
