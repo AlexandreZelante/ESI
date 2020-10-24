3b92994b5305215f410ea130284f97b10bf11381
Igor Antun <igor@antun.me>
2020-10-24 16:08:50 -0300

    feat: install cocoon and jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require("webpack");
+
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.append(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+  })
+);
+
+
 module.exports = environment
