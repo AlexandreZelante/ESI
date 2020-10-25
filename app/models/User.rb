33a059ef3c3a015da8b0879fa25b5851ead38667
GuilhermeOliGou <guilherme.oligou@gmail.com>
2020-10-24 22:38:41 -0300

    batata

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-	validates :email, :password, confirmation: true
-	validates :name, :email, :email_confirmation, :cpf, :birth_date, :phone, :gender, :password, presence: true
+	#validates :email, :password, confirmation: true
+	validates :name, :email, :cpf, :birth_date, :phone, :gender, :password, presence: true
 end
