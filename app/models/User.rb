a34a535427dac336fc8a86adf04ba6d676928372
GuilhermeOliGou <guilherme.oligou@gmail.com>
2020-10-25 03:05:19 -0300

    m

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	validates :email, confirmation: { case_sensitive: false }
-	validates :email_confirmation, presence: true
+	validates :email, confirmation: true
+	validates :email_confirmation, presence: false
 	validates :name, :email, :cpf, :birth_date, :phone, :gender, :password, presence: true
 end
