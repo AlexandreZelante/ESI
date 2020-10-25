efdfe290d841e659b2b51db88c194d08818438c1
GuilhermeOliGou <guilherme.oligou@gmail.com>
2020-10-25 18:27:30 -0300

    m

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 	validates :email, confirmation: true
-	validates :email_confirmation, presence: false
+	validates :email_confirmation, presence: true
 	validates :name, :email, :cpf, :birth_date, :phone, :gender, :password, presence: true
 end
