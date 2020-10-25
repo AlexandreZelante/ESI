569145614eaa219737c5e5428e0c7655da67d417
GuilhermeOliGou <guilherme.oligou@gmail.com>
2020-10-25 19:23:10 -0300

    Validação do cadastro de usuário

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 	validates :email, confirmation: true
 	validates :password, confirmation: true
+	validates :cpf, :phone, numericality: { only_integer: true }
 	validates :email_confirmation, presence: true
 	validates :name, :email, :cpf, :birth_date, :phone, :gender, :password, presence: true
 end
