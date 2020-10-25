9ffce284d5d100405c58a8f98e45ee4738768c6a
GuilhermeOliGou <guilherme.oligou@gmail.com>
2020-10-25 20:01:03 -0300

    m

--- app/models/User.rb
+++ app/models/User.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 	validates :email, confirmation: true
 	validates :password, confirmation: true
 	validates :cpf, :phone, numericality: { only_integer: true }
-	validates :email_confirmation, presence: true
-	validates :name, :email, :cpf, :birth_date, :phone, :gender, :password, presence: true
+	#validates :email_confirmation, presence: true
+	#validates :name, :email, :cpf, :birth_date, :phone, :gender, :password, presence: true
 end
