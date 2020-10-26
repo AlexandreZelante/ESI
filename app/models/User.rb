a437c2921dbcc8e68c6d44d08811e64163ac236e
Guilherme Oliveira Goularte <guilherme.oligou@gmail.com>
2020-10-25 23:37:01 -0300

    Validacao usuario (#14)

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,3 +1,8 @@
+
 class User < ActiveRecord::Base
+	  validates :email, confirmation: true
+	  validates :password, confirmation: true
+	  validates :cpf, :phone, numericality: { only_integer: true }
+	  validates :gender, presence: true
     has_secure_password
 end
