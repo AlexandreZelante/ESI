a0ddc3d0e9653bf8889591a3e14743dc4927cdcb
GuilhermeOliGou <guilherme.oligou@gmail.com>
2020-11-09 02:01:59 -0300

    8

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,8 +19,6 @@ class User < ActiveRecord::Base
 
 	private
 	def create_confirmation_token
-        if self.confirm_token.blank?
-            self.confirm_token = SecureRandom.urlsafe_base64.to_s
-        end
+        self.confirm_token = SecureRandom.urlsafe_base64.to_s
     end
 end
