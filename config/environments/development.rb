7f6c1c2f2b1273bd04c9510cf0af3dfb5c6dc31f
GuilhermeOliGou <guilherme.oligou@gmail.com>
2020-11-21 19:27:42 -0300

    Default URL fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
   Rails.application.routes.default_url_options[:host] = 'esi-2020.herokuapp.com'
 
   config.action_mailer.delivery_method = :smtp
