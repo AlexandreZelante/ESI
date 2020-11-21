49ee51c84db72173e5b95df9b700bc700bf80852
GuilhermeOliGou <guilherme.oligou@gmail.com>
2020-11-21 19:22:51 -0300

    default URL fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  Rails.application.routes.default_url_options[:host] = 'salty-coast-47233.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = 'esi-2020.herokuapp.com'
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
