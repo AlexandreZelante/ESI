559fe1a330b3c516e287b444b1d421db647b4fee
PedroSillos <pedro.sillos@gmail.com>
2020-12-07 02:00:40 -0300

    mais um commit squashado

--- features/step_definitions/edit_workout_comment_step.rb
+++ features/step_definitions/edit_workout_comment_step.rb
@@ -1,11 +1,3 @@
-Dado("que eu tenho um comentário de treino cadastrado") do
-    workout_comment = WorkoutComment.new
-    workout_comment.text = "Um comentário comentado",
-    workout_comment.workout_id = 1,
-    workout_comment.user_id = 1
-    workout_comment.save
-end
-
 Quando ("clico no link de Editar") do
     click_on "Editar"
 end
